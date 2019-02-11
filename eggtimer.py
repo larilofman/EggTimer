aa2d8653fe0ba99ebc7de420da286858e1c884e7
Lari Löfman <larwazor@gmail.com>
2019-02-11 20:32:58 +0200

    Add digit validation

--- eggtimer.py
+++ eggtimer.py
@@ -62,16 +62,16 @@ class timer():
     """Handles user input and states."""
 
     def __init__(self, root):
-        self.time = self.get_time_from_file()
+        self.seconds = self.get_seconds_from_file()
         self.state = None
         self.root = root
         self.change_state_to_set()
 
-    def get_time_from_file(self):
-        """Returns time from time.json or returns 240 on exception."""
+    def get_seconds_from_file(self):
+        """Returns seconds from json or 240 on exception."""
         try:
             with open('time.json') as f:
-                return json.load(f)['time']
+                return json.load(f)['seconds']
         except:
             return 240
 
@@ -89,28 +89,44 @@ class state_set():
     def __init__(self, timer):
         global font_name
         self.timer = timer
-        print(timer.time)
+        print(timer.seconds)
         self.font_name = font_name
         self.init_elements()
 
     def init_elements(self):
 
         Label(self.timer.root, text="Set time",
-              font=(self.font_name, 24)).grid(row=0, columnspan=self.timer.get_root_width())
+              font=(self.font_name, 24)).grid(row=0, columnspan=self.timer.get_root_width(), pady=(30, 0))
+
         element_frame = Frame(self.timer.root)
         element_frame.grid(row=1, columnspan=self.timer.get_root_width())
         setup_grid(element_frame, 5, 4)
+
         hours = self.add_digit_input(element_frame, 'h', 1)
         minutes = self.add_digit_input(element_frame, 'min', 2)
         seconds = self.add_digit_input(element_frame, 's', 3)
 
+        button_run = Button(self.timer.root, height=1,
+                            width=5, font=(self.font_name, 24), text='Start')
+        button_run.grid(
+            row=2, columnspan=self.timer.get_root_width(), pady=(0, 30))
+
+    def digit_callback(self, P):
+        if len(P) < 3 and str.isdigit(P) or P == "":
+            return True
+        else:
+            return False
+
     def add_digit_input(self, frame, text, object_index):
+
+        vcmd = (frame.register(self.digit_callback))
+
+        digit_field = Entry(frame, width=2, font=(
+            self.font_name, 24), validate='all', validatecommand=(vcmd, '%P'), justify=CENTER)
+        digit_field.grid(row=1, column=object_index, padx=5)
 
         button_plus = Button(frame, height=2, width=5, text="+")
         button_plus.grid(row=0, column=object_index, padx=5)
-
-        digit_field = Entry(frame, width=2, font=(self.font_name, 24))
-        digit_field.grid(row=1, column=object_index, padx=5)
 
         button_minus = Button(frame, height=2, width=5, text="-")
         button_minus.grid(row=2, column=object_index, padx=5)
