f801a89551b254a708b79e999cd3f20f42f5bf6a
Lari Löfman <larwazor@gmail.com>
2019-02-11 17:04:34 +0200

    Add possibility to set fixed grid size

--- eggtimer.py
+++ eggtimer.py
@@ -2,6 +2,9 @@ import time
 import json
 from tkinter import *
 
+# Font used application wide
+font_name = "Verdana"
+
 # Timer:
 # - keeps user defined time saved in a settings file
 # - time is handled as seconds
@@ -29,25 +32,34 @@ from tkinter import *
 
 
 def init_app():
-    """
-    Sets up application window and starts loop.
-    """
+    """Sets up application window and starts loop."""
 
     root = Tk()
     root.geometry("480x480")
     root.resizable(False, False)
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
+
+    setup_grid(root, 5, 3)
+
     root.title('EggTimer')
     root.call('wm', 'iconphoto', root._w, PhotoImage(file='icon.png'))
     maintimer = timer(root)
     root.mainloop()
 
 
+def setup_grid(element, columns, rows):
+    """Create a grid of the size columns*rows on element."""
+
+    for c in range(0, columns):
+        element.columnconfigure(c, weight=1)
+
+    for r in range(0, rows):
+        element.rowconfigure(r, weight=1)
+
+    print(element.grid_size())
+
+
 class timer():
-    """
-    Handles user input and states.
-    """
+    """Handles user input and states."""
 
     def __init__(self, root):
         self.time = self.get_time_from_file()
@@ -56,9 +68,7 @@ class timer():
         self.change_state_to_set()
 
     def get_time_from_file(self):
-        """
-        Returns time from time.json or returns 240 on exception.
-        """
+        """Returns time from time.json or returns 240 on exception."""
         try:
             with open('time.json') as f:
                 return json.load(f)['time']
@@ -68,40 +78,45 @@ class timer():
     def change_state_to_set(self):
         self.state = state_set(self)
 
+    def get_root_width(self):
+        """Returns amount of columns on the root object"""
+        return self.root.grid_size()[0]
+
 
 class state_set():
-    """
-    State for setting the timer.
-    """
+    """State for setting the timer."""
 
     def __init__(self, timer):
+        global font_name
         self.timer = timer
         print(timer.time)
+        self.font_name = font_name
         self.init_elements()
 
     def init_elements(self):
 
-        element_frame = Frame(self.timer.root, bg="brown", padx=120, pady=80,
-                              width=480, height=480)
-        Label(element_frame, text="Set time",
-              font=('Verdana', 24)).grid(row=0, column=1)
-        Frame(element_frame, height=100).grid(row=1, column=1)
-        hours = self.add_digit_input(element_frame, 0)
-        minutes = self.add_digit_input(element_frame, 1)
-        seconds = self.add_digit_input(element_frame, 2)
+        Label(self.timer.root, text="Set time",
+              font=(self.font_name, 24)).grid(row=0, columnspan=self.timer.get_root_width())
+        element_frame = Frame(self.timer.root)
+        element_frame.grid(row=1, columnspan=self.timer.get_root_width())
+        setup_grid(element_frame, 5, 4)
+        hours = self.add_digit_input(element_frame, 'h', 1)
+        minutes = self.add_digit_input(element_frame, 'min', 2)
+        seconds = self.add_digit_input(element_frame, 's', 3)
 
-        element_frame.grid(ipady=60)
-
-    def add_digit_input(self, frame, object_index):
+    def add_digit_input(self, frame, text, object_index):
 
         button_plus = Button(frame, height=2, width=5, text="+")
-        button_plus.grid(row=2, column=object_index, padx=5)
+        button_plus.grid(row=0, column=object_index, padx=5)
 
-        digit_field = Entry(frame, width=7)
-        digit_field.grid(row=3, column=object_index, padx=5)
+        digit_field = Entry(frame, width=2, font=(self.font_name, 24))
+        digit_field.grid(row=1, column=object_index, padx=5)
 
         button_minus = Button(frame, height=2, width=5, text="-")
-        button_minus.grid(row=4, column=object_index, padx=5)
+        button_minus.grid(row=2, column=object_index, padx=5)
+
+        Label(frame, text=text, font=(self.font_name, 12)).grid(
+            row=4, column=object_index)
 
 
 init_app()
